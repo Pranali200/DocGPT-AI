284e25f2cb77abafb961f22c6cfc77a3c7b9f7a1
Pranali200 <hemanepranali08@gmail.com>
2026-09-16 01:02:27 +0530

    final fix

--- chatbot-frontend/src/pages/Signup.jsx
+++ chatbot-frontend/src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { signup } from "../services/authService";
 import { useNavigate, Link } from "react-router-dom";
 
-import "./Auth.css";
+import "./auth.css";
 
 
 function Signup() {
